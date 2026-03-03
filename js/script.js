47a2626b20f88c05949e058e355493fd7d9a1f94
mstmehotabin0329-ops <mstmehotabin0329@gmail.com>
2026-03-03 23:02:30 +0600

    added all jobs card

--- js/script.js
+++ js/script.js
@@ -100,3 +100,63 @@ let cards = [
 
 let interviewList = [];
 let rejectedList = [];
+
+const allCards = document.getElementById('allCards');
+const totalCount = document.getElementById('total');
+const interviewCount = document.getElementById('interview');
+const rejetedCount = document.getElementById('rejected');
+const availableCount = document.getElementById('availableCount');
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+//  calculate  count
+function calculateCount() {
+    totalCount.innerText = cards.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+    availableCount.innerText = cards.length;
+
+}
+calculateCount();
+
+
+
+
+//  
+function getAllData() {
+    allCards.innerHTML = '';
+
+    for (let jobCard of cards) {
+        let div = document.createElement('div');
+        div.className = 'bg-base-100 shadow rounded-xl p-6 flex justify-between mb-5 mt-8'
+        div.innerHTML = `
+        <div class="space-y-6">
+            <div class="space-y-2">
+                <p class="companyName text-2xl font-semibold text-[#002C5C]">${jobCard.company}</p>
+                <p class="position text-[#64748B]">${jobCard.position}</p>
+            </div>
+            <p class="salary text-[#64748B]"><span>${jobCard.jobCategory}</span> • <span>${jobCard.workLocation}</span> • <span>${jobCard.minSalary}</span> - <span>${jobCard.maxSalary}</span></p>
+            <div class="space-y-2">
+                <p class="type bg-gray-200 inline-block py-2 px-4 font-semibold rounded-md">${jobCard.status}</p>
+                <p class="notes text-[#64748B]">${jobCard.description}</p>
+            </div>
+            <div class="flex gap-3">
+                <button class="inverviw-btn btn btn-outline btn-success">INTERVIEW</button>
+                <button class="rejected-btn btn btn-outline btn-error">REJECTED</button>
+            </div>
+        </div>
+        <div onclick="removeData('${jobCard.id}')" class="mt-5 mr-5">
+            <span class="deleted cursor-pointer text-[#64748B] hover:bg-red-200 border border-gray-300 rounded-full p-2"><i
+                    class="fa-regular fa-trash-can"></i></span>
+        </div>
+        `
+        allCards.appendChild(div);
+    }
+    calculateCount();
+    
+}
+
+
+
+
